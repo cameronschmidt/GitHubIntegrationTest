69d2c8e3807015b480ada64868693160b618ca0d
CS <cameronschmidt@users.noreply.github.com>
2016-12-06 16:07:59 +0100

    Create HelloWorld.js

---
+++ src/HelloWorld.js
@@ -0,0 +1,8 @@
+
+test = new Date()
+month = test.getMonth()
+month = (month * 1) + 1
+day = test.getDate()
+year = test.getFullYear()
+
+document.write("Hello, world, the date is ",day,"/",month,"/",year,"!" )
