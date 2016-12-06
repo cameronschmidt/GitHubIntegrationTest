55cd37bd88326c88690af7e7513df4fe61aa4eb1
CS <cameronschmidt@users.noreply.github.com>
2016-12-06 16:09:23 +0100

    Create HelloTwice.js

---
+++ src/HelloTwice.js
@@ -0,0 +1,8 @@
+
+test = new Date()
+month = test.getMonth()
+month = (month * 1) + 1
+day = test.getDate()
+year = test.getFullYear()
+
+document.write("Hello for the second time, world, the date is ",day,"/",month,"/",year,"!" )
