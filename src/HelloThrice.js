e2877bd0986ef7cc6e8a97f1ef3822f3b11201be
CS <cameronschmidt@users.noreply.github.com>
2016-12-06 16:09:56 +0100

    Create HelloThrice.js

---
+++ src/HelloThrice.js
@@ -0,0 +1,8 @@
+
+test = new Date()
+month = test.getMonth()
+month = (month * 1) + 1
+day = test.getDate()
+year = test.getFullYear()
+
+document.write("Hello, world, for the third and final time the date is ",day,"/",month,"/",year,"!" )
