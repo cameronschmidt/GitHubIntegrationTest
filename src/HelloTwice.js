61b74df0727ced2c3cba63a2a5b512785859a3a8
CS <cameronschmidt@users.noreply.github.com>
2016-12-07 13:57:52 +0100

    Update HelloTwice.js

--- src/HelloTwice.js
+++ src/HelloTwice.js
@@ -6,17 +6,8 @@ month = (month * 1) + 1
 day = test.getDate()
 year = test.getFullYear()
 
-<<<<<<< Updated upstream
-if (year === NaN) {
-  document.write("Year is not a number!")
-}
-
-document.write("Hello for the second time, world, the date is ",day,"/",month,"/",year,"!" )
-=======
 if (year !== year) {
   console.log("year is not a number");
 }
 
 document.write("US date format: ",month,"/",day,"/",year," ")
-
->>>>>>> Stashed changes
