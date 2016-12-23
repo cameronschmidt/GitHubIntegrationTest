e80b2d0bad2dfc4d55a9e88c0705741ec7957df2
CS <cameronschmidt@Cameron.gva.sonarsource.com>
2016-12-23 09:42:13 +0100

    Added another FIXME
    Added another FIXME

--- src/HelloWorld.js
+++ src/HelloWorld.js
@@ -2,7 +2,7 @@
 let test = new Date()
 let month = test.getMonth()
 month = (month * 1) + 1 // FIXME - this looks incorrect!
-let day = test.getDate()
+let day = test.getDate() //FIXEME - this can't be right!
 let year = test.getFullYear()
 
 document.write("Hello, world, the date is ",day,"/",month,"/",year,"!" )
