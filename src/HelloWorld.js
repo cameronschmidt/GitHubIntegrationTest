33fc2699b9047f65c5120361e0e543f601b748dd
CS <cameronschmidt@users.noreply.github.com>
2016-12-23 10:00:28 +0100

    Update HelloWorld.js

--- src/HelloWorld.js
+++ src/HelloWorld.js
@@ -3,6 +3,6 @@ let test = new Date()
 let month = test.getMonth()
 month = (month * 1) + 1 // FIXME - this looks incorrect!
 let day = test.getDate() //FIXEME - this can't be right!
-let year = test.getFullYear()
+year = test.getFullYear() // FIXME - should use a let statement
 
 document.write("Hello, world, the date is ",day,"/",month,"/",year,"!" )
